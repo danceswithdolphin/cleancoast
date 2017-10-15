f7adf0c6d6bfcc99d014cdb76fc8463940dbdb92
Joseph Bonds <danceswithdolphin@gmail.com>
2017-10-15 18:57:19 -0400

    cleanup

--- date_play.js
+++ date_play.js
@@ -2,8 +2,13 @@ var first_line='';
 var today_yyyy = '';
 var today_mm = '';
 var today_dd = '';
+var today_hr = '';
+var today_min = '';
 var x=0;
 var y=0;
+
+var now = Date();
+console.log('now= '+now);
 
 var readline = require('readline'),
     rl = readline.createInterface(process.stdin, process.stdout);
@@ -17,7 +22,9 @@ today_mm = today_mm.substring(today_mm.length -2);
 today_dd = first.getDate();
 today_dd = "0"+today_dd;
 today_dd = today_dd.substring(today_dd.length - 2);
-first_line = today_yyyy+'.'+today_mm+'.'+today_dd;
+today_hr = first.getHours();
+today_min = first.getMinutes();
+first_line = today_yyyy+'.'+today_mm+'.'+today_dd+' '+today_hr+':'+today_min;
 console.log('today is ' + first_line);
 first=new Date(first_line);
 x=first.getTime();
